a007f85a343316d680d2bdd895c11e176cf28ad7
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-05-04 17:49:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,41 @@
- provider "azurerm" {
-  features {}
+terraform {
+  required_version = ">= 0.11.1"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
+variable "location" {
+  description = "Azure location in which to create resources"
+  default = "East US"
 }
 
-module "linuxservers" {
+variable "windows_dns_prefix" {
+  description = "DNS prefix to add to to public IP address for Windows VM"
+}
+
+variable "admin_password" {
+  description = "admin password for Windows VM"
+  default = "pTFE1234!"
+}
+
+module "windowsserver" {
   source              = "Azure/compute/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-}
-
-module "windowsservers" {
-  source              = "Azure/compute/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  is_windows_image    = true
-  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-  admin_password      = "ComplxP@ssw0rd!"
+  version             = "1.1.5"
+  location            = "${var.location}"
+  resource_group_name = "${var.windows_dns_prefix}-rc"
+  vm_hostname         = "pwc-ptfe"
+  admin_password      = "${var.admin_password}"
   vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
+  public_ip_dns       = ["${var.windows_dns_prefix}"]
+  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
 }
 
 module "network" {
   source              = "Azure/network/azurerm"
-  version             = "3.0.0"
-  resource_group_name = azurerm_resource_group.example.name
-  allow_rdp_traffic   = "true"
-  allow_ssh_traffic   = "true"
-  subnet_prefixes     = ["10.0.1.0/24"]
-
+  version             = "1.1.1"
+  location            = "${var.location}"
+  resource_group_name = "${var.windows_dns_prefix}-rc"
+  allow_ssh_traffic   = true
 }
 
-output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_dns_name
+output "windows_vm_public_name"{
+  value = "${module.windowsserver.public_ip_dns_name}"
 }
-
-output "windows_vm_public_name" {
-  value = module.windowsservers.public_ip_dns_name
-}  
