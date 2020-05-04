bb5b2dcdd95ecbf1ca7d6bf2fc0c4a4ef125d89a
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-05-04 16:53:39 -0400

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,1 +1,37 @@
+resource "azurerm_resource_group" "test" {
+  name     = "${var.windows_dns_prefix}-rc"
+  location = "East US"
+}
 
+module "network-security-group" {
+  source  = "app.terraform.io/larryebaum-demo/network-security-group/azurerm"
+  version = "3.0.1"
+  resource_group_name = azurerm_resource_group.test.name
+  security_group_name   = "nsg"
+  source_address_prefix = ["${module.windowsservers.public_ip_address}"]
+  predefined_rules = [
+    {
+      name     = "SSH"
+      priority = "500"
+    },
+    {
+      name              = "LDAP"
+      source_port_range = "1024-1026"
+    }
+  ]
+  custom_rules = [
+    {
+      name                   = "myhttp"
+      priority               = "200"
+      direction              = "Inbound"
+      access                 = "Allow"
+      protocol               = "tcp"
+      destination_port_range = "8080"
+      description            = "description-myhttp"
+    }
+  ]
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+}
