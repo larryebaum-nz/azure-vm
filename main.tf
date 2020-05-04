1259c5e9d5eba2f493a6ad14b3b62860585004eb
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-05-04 18:41:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,3 +39,20 @@ module "network" {
 output "windows_vm_public_name"{
   value = "${module.windowsserver.public_ip_dns_name}"
 }
+
+module "database" {
+  source  = "app.terraform.io/larryebaum-demo/database/azurerm"
+  version = "1.1.0"
+  resource_group_name = "myapp"
+  location = "eastus"
+  db_name = "mydatabase"
+  sql_admin_username  = "mradministrator"
+  sql_password        = "P@ssw0rd12345!"  
+  tags             = {
+                        environment = "dev"
+                        costcenter  = "it"
+                      }  
+}
+
+  
+  
