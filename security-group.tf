4fec6c30adf6cc7c6edcca6a9b9d23a2f207c953
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-05-04 17:01:37 -0400

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,7 +1,7 @@
 module "disksnapshot" {
   source               = "Azure/disk-snapshot/azurerm"
   resource_group_name  = "resourcegroup1"
-  version              = "0.1"
+  version              = "0.1.0"
   managed_disk_names   = ["disk1", "disk2"]
 
   tags = {
